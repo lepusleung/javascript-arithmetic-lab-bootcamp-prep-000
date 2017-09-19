62df404e20ece7fede9be80ea774f5204087beb3
Man Leung <lepusleung@gmail.com>
2017-09-19 20:59:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(n) 
+
+function dec (n)
+
+function makeInt(n)
+
+function makeInt(n)
